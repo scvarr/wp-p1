0362d48a5835dc4634433c1947752a9dafaf60fb
kutorov-ms <kutorov-ms@rosatomflot.com>
2023-11-03 13:56:42 +0300

    0311

---
+++ scripts/express.js
@@ -0,0 +1,54 @@
+
+const nameInput = document.getElementById("input-name");
+const passInput = document.getElementById("input-password");
+const passMoreInput = document.getElementById("input-password-more")
+let select = document.querySelector('select');
+//const userType = document.getElementById("user-type").index
+//let userTypeValue;
+let nameInputValue;
+let passInputValue;
+let passMoreInputValue;
+let userType;
+nameInput.addEventListener("input", ()=> {
+  nameInputValue = nameInput.value
+})
+nameInput.oninput = function () {
+    document.getElementById("input-password").disabled = nameInputValue.length < 3;
+}
+nameInput.addEventListener("blur", () => {
+    if (nameInputValue.length <3) {
+        window.alert("Имя должно быть не меньше трех символов")
+    }
+})
+passInput.addEventListener("input", () => {
+    passInputValue=passInput.value;
+})
+passInput.addEventListener("blur", ()=>{
+    if (passInputValue.length <6) {
+        window.alert("Пароль должен быть не меньше 6 символов")
+    }
+})
+passInput.oninput = function () {
+    document.getElementById("input-password-more").disabled = passInputValue.length <= 5;
+}
+passMoreInput.addEventListener("input", ()=> {
+    passMoreInputValue = passMoreInput.value;
+})
+passMoreInput.addEventListener("blur", ()=>{
+    if (passInputValue !== passMoreInputValue) {
+        window.alert("Введеные пароли не совпадают")
+        document.getElementById("input-password").value = "";
+        document.getElementById("input-password-more").value = "";
+        document.getElementById("input-password").focus()
+        document.getElementById("user-type").disabled = true;
+    } else {
+        document.getElementById("user-type").disabled = false;
+    }
+})
+passMoreInput.oninput = function () {
+    document.getElementById("user-type").disabled = passInputValue !== passMoreInputValue
+}
+select.addEventListener('change', ()=>{
+    userType = select.value;
+    document.getElementById("button-submit").disabled = false;
+})
