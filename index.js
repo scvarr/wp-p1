8fcdf184a0395e092526ec432f6d5b04677c5700
scvarr <scvarr@gmail.com>
2023-11-04 17:13:17 +0300

    index.html start

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-
+app.get('/registration', (req, res) =>{
+    res.sennpmdFile("index.html")
+    })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
